445f185e25fab0aebc133c200241548dc4c7f007
Sergiy Dybskiy <sergiy@minoeworks.com>
2013-12-22 20:10:13 -0500

    added edit function

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -6,5 +6,23 @@ Meteor.Router.add({
 		and: function(id) { Session.set('currentPostId', id); }
 	},
 
+	'/posts/:_id/edit': {
+		to: 'postEdit',
+		and: function(id) { Session.set('currentPostId', id); }
+	},
+
 	'/submit': 'postSubmit'
 });
+
+Meteor.Router.filters({
+	'requireLogin': function(page) {
+		if (Meteor.user())
+			return page;
+		else if (Meteor.loggingIn())
+			return 'loading';
+		else
+			return 'accessDenied';
+	}
+});
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
